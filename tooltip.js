87087ebc87e6121e75bd96903af7f49eb5ad0de7
tomo <cxytomo@gmail.com>
2012-12-25 19:50:54 +0800

    coarse tooltip
    coarse tooltip which works sensitively enough and its tooltip-arrow is
    not born yet.

---
+++ tooltip.js
@@ -0,0 +1,123 @@
+!function ($) {
+	var Tooltip = function (e) {};
+	Tooltip.prototype = {
+		constructor: Tooltip
+
+	,	posClass: 'top'
+
+	,	enter: function (e) {
+			this.$e = e || window.event;
+			this.$target = e.target || e.srcElement;
+			return this.getPos();
+		}
+
+	, getPos: function (e) {
+			var tar
+			,	tarBox
+			,	width
+			,	height;
+			e = this.$e;
+			tar = this.$target;
+			tarBox = tar.getBoundingClientRect();
+			this.tarTop = tarBox.top;
+			this.tarBottom = tarBox.bottom;
+			this.tarWidth = tarBox.width;
+			this.tarHeight = tarBox.height;
+			return this.tip();
+		}
+
+	,	tip: function () {
+			var template
+			,	arrow
+			,	inner;
+			template = document.createElement('div');
+			arrow = document.createElement('div');
+			inner = document.createElement('div');
+			template.className = "tooltip";
+			arrow.className = "tooltip-arrow";
+			inner.className= "tooltip-inner";
+			template.appendChild(arrow);
+			template.appendChild(inner);
+			this.$tip = template;
+			return this.setPos();
+		}
+
+	,	setPos: function () {
+			var toTop
+			,	toLeft
+			,	tip
+			,	tipBox
+			,	twidth
+			,	theight
+			,	left
+			,	top
+			,	tar
+			,	pos;
+			tar = $(this.$target);
+			toTop = tar.position().top;//position()Get the current coordinates of the first element in
+																//the set of matched elements, relative to the offset parent.
+			toLeft = tar.position().left;
+			this.setContent();
+			this.$target.parentNode.appendChild(this.$tip);
+			tip = document.getElementsByClassName('tooltip')[0];
+			this.tipBox = tip.getBoundingClientRect();
+			this.tipWidth = this.tipBox.width;
+			this.tipHeight = this.tipBox.height;
+			pos = this.posClass;
+			if(this.tarTop >= this.tipHeight) {
+				pos = "top";
+			}
+			else {
+				pos = "bottom";
+			}
+			tip.className = 'tooltip ' + pos;
+			switch(pos) {
+				case "top":
+					top = toTop - this.tipHeight -2;
+					left = toLeft + this.tarWidth/2 - this.tipWidth/2;
+					break;
+				case "bottom":
+					top = toTop + this.tarHeight + 2;
+					left = toLeft + this.tarWidth/2 - this.tipWidth/2;
+					break;
+			}
+			$('.tooltip')[0].style.top = top + "px";
+			$('.tooltip')[0].style.left = left + "px";
+			return this.show();
+		}
+
+	,	setContent: function () {
+			var dataset
+			,	cont
+			,	insCont;
+			dataset = this.$target.dataset;
+			cont = dataset.cont;
+			insCont = document.createTextNode(cont);
+			this.$tip.childNodes[1].appendChild(insCont);
+		}
+
+	,	show: function (e) {
+			$('.tooltip').fadeTo('slow',0.8);
+		}
+
+	,	leave: function (e) {
+			var tip;
+			e = e || window.event;
+			tip = this.$tip;
+			return $('.tooltip').fadeOut('fast',function(){
+				tip.parentNode.removeChild(tip);
+			});
+		}
+
+	,	hide: function (e) {
+			this.$tip.parentNode.removeChild(this.$tip);
+		}
+	}
+	var tooltip = new Tooltip();
+	$('.tip a').mouseover(function (e) {
+		tooltip.enter(e);
+	});
+	$('.tip a').mouseleave(function (e) {
+		tooltip.leave();
+	});
+}(window.jQuery);
